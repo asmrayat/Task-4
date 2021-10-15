6330fed4adda15df6cac5baec58d0e2f9a39379a
asmrayat <asmrayat@gmail.com>
2021-10-15 22:25:20 +0600

    first commit

---
+++ src/rayat/AdvancedArithmetic.java
@@ -0,0 +1,18 @@
+package rayat;
+
+import java.math.BigInteger;
+
+public interface AdvancedArithmetic {
+
+    int divisorSum(int n);
+    BigInteger findFactorial(int n);
+
+    /*
+    Name: Abu Shalehin MD.Rayat
+    ID: 2012020162
+    Section: D
+    Email: cse_2012020162@lus.ac.bd
+    Date: 15-10-2021
+
+    */
+}
---
+++ src/rayat/Main.java
@@ -0,0 +1,33 @@
+package rayat;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        Calculator calculator =new Calculator();
+
+        System.out.print("Enter the number : ");
+        int a = input.nextInt();
+        int sum = calculator.divisorSum(a);
+        System.out.println("The sum of divisors of the number " + a + " is: " + sum + "\n");
+
+        System.out.print("Enter the number for factorial : ");
+        int b = input.nextInt();
+        BigInteger factorial = calculator.findFactorial(b);
+        System.out.println("The factorial of the number " + b + " is: " + factorial);
+    }
+
+    /*
+    Name: Abu Shalehin MD.Rayat
+    ID: 2012020162
+    Section: D
+    Email: cse_2012020162@lus.ac.bd
+    Date: 15-10-2021
+
+    */
+
+}
